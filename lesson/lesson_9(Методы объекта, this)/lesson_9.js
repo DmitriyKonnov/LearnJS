f737feab518bff53027209d626afb330fb1c1884
DmitriyKonnov <dimon170893@gmail.com>
2021-02-17 18:04:28 +0300

    added lesson_9

---
+++ lesson/lesson_9(Методы объекта, this)/lesson_9.js
@@ -0,0 +1,23 @@
+// Создайте объект calculator (калькулятор) с тремя методами:
+
+// read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+// sum() (суммировать) возвращает сумму сохранённых значений.
+// mul() (умножить) перемножает сохранённые значения и возвращает результат.
+
+let calculator = {
+    // ... ваш код ...
+    read() {
+        this.x = +prompt('x', '');
+        this.y = +prompt('y', '');
+    },
+    sum() {
+        return this.x + this.y;
+    },
+    mul() {
+        return this.x * this.y;
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
